5e8ec14e9743b39ef2f1562d55d554fa0f025425
KulcsRX21 <kulcsrx@gmail.com>
2022-10-24 19:56:38 +0200

    module 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { Http, Response } from '@angular/http'
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
