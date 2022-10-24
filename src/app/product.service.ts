6bab283cc753fa09157df656c979affc8aea7907
KulcsRX21 <kulcsrx@gmail.com>
2022-10-24 19:51:37 +0200

    module 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,7 +7,7 @@ import { Http, Response } from '@angular/http'
   providedIn: 'root'
 })
 export class ProductService {
-  _albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: HttpClient) { }
 
